a58ca8aebc5152ad883382694aa00cd24d454015
Nergo Kyn <nergokyn@gmail.com>
2024-03-12 23:03:43 +0100

    Initial Commit

---
+++ MyHabits/MyHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHabitsApp.swift
+//  MyHabits
+//
+//  Created by M M on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MyHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
